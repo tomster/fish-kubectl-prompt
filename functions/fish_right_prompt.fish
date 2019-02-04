639eeea3a57fbc3847439cd13d56dc2387183deb
Tom Lazar <tomster@pyfidelity.com>
2019-02-04 10:33:09 +0100

    Display the name of the current context only

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -2,9 +2,7 @@ function kubectl_status
   [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
   [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
   set -l ctx (get_kubectl_context) 
-  set -l cluster $ctx[1]
-  set -l ns $ctx[2]
-  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($cluster$KUBECTL_PROMPT_SEPARATOR$ns)"
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx)"
 end
 
 function fish_right_prompt
