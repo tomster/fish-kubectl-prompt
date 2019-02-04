28d4a29d7471659a81a0c24438472808349f5141
Tom Lazar <tomster@pyfidelity.com>
2019-02-04 10:32:28 +0100

    Don't insist on a resolvable, single location
    if the config contains multiple paths this restriction falsely returns
    no config

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,13 +1,6 @@
 function kubectl_status
   [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
   [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
-  set -l config $KUBECONFIG
-  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
-  if [ ! -f $config ]
-    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
-    return
-  end
-
   set -l ctx (get_kubectl_context) 
   set -l cluster $ctx[1]
   set -l ns $ctx[2]
